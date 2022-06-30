3b53c6a89b240074a93abf568428021dc3136cda
Matthew <matthewgonzalez2324@gmail.com>
2022-06-30 15:46:56 -0700

    Add input validation
    Clean up console output to make it more useful after each round
    of play.

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ function computerPlay(){
 function playRound(playerSelection, computerSelection){
     // game variations
     playerSelection = capitalizeFirstChar(playerSelection);
+    isValidated = validateInput(playerSelection);
 
     if (playerSelection === computerSelection){
         outcome = "draw";
@@ -35,40 +36,73 @@ function playRound(playerSelection, computerSelection){
 
 // Play a five round game that keeps score
 // and reports a winner/loser at the end.
-function game(playerSelection, computerSelection) {
+function game() {
     playerScore = 0;
     computerScore = 0;
+    whoWon = null;
     for (let i = 0; i < 5; i++){
-        let playerSelection = prompt("Rock, Paper, or Scissors?", "Rock");
+        let playerSelection = playerSelect();
         let computerSelection = computerPlay();
         outcome = playRound(playerSelection, computerSelection);
-        console.log("Round " + String(i+1) + ": " + outcome);
         if (outcome === "win"){
             playerScore++;
+            whoWon = "Player";
         }
         else if (outcome === "lose"){
             computerScore++;
+            whoWon = "Computer"
         }
+        else{
+            whoWon = "Tie"
+        }
+        console.log("Round: " + String(i+1) + "\n"
+            + "Player Chose: " + playerSelection + "\n"
+            + "Computer Chose: " + computerSelection + "\n"
+            + whoWon + " won the round");
     }
     if (playerScore === computerScore){
-        console.log("It's a draw!");
+        results = "It's a draw!";
     }
     else if (playerScore > computerScore){
-        console.log("Player wins!");
+        results = "Player wins!";
     }
     else{
-        console.log("CPU wins!")
+        results = "CPU wins!";
     }
+    console.log("Results: " + results);
 }
 
+// Prompts user selection
+function playerSelect(){
+    playerSelection = promptPlayerSelection();
+    playerSelection = capitalizeFirstChar(playerSelection);
+    validatedPlayerSelection = validateInput(playerSelection); 
+    
+    return validatedPlayerSelection;
+}
 
 // Helper function(s)
-function capitalizeFirstChar(myString){
-    return myString.charAt(0).toUpperCase() + myString.toLowerCase().slice(1);
+function promptPlayerSelection(message="Type Rock, Paper, or Scissors"){
+    playerSelection = prompt(message, "Rock");
+    return playerSelection;
 }
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-// console.log("computer selection", computerSelection);
-// console.log(playRound(playerSelection, computerSelection));
-game(playerSelection, computerSelection);
+function capitalizeFirstChar(myString){
+    if (myString !== null && myString !== ""){
+        myString = myString.charAt(0).toUpperCase() + myString.toLowerCase().slice(1);
+    }
+
+    return myString;
+}
+
+// Validates user input
+function validateInput(userInput){
+    while (userInput !== "Rock" && userInput !== "Paper" && userInput !== "Scissors"){
+        userInput = promptPlayerSelection(message="Try again:\nType Rock, Paper, or Scissors");
+    }
+
+    return userInput;
+}
+
+
+game();
